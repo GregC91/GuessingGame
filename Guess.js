1658923525ee870f925700043015c11acfdeb08f
GregC91 <g_chambers1801@icloud.com>
2016-04-21 16:19:50 +0100

    Finshing CSS

---
+++ Guess.js
@@ -0,0 +1,1 @@
+var guessGame = docu
--- GuessingGame.js
+++ GuessingGame.js
@@ -1,28 +1,31 @@
 //Build a small game where the user has to guess a secret number which is generated automatically in Javascript. After each guess the webpage displays to the user whether their guess is too large or too small. When the user guesses the secret number correctly - three pieces of information are displayed on the webpage: (1) the correct number  (2)the number of guesses (3)the message “Well done – correct guess!”
 //event listener
-var guessGame = document.getElementById("compare");
-guessGame.addEventListener("click",compare,false);
+/*var guessGame = document.getElementById("compare");
+guessGame.addEventListener("click",compare,false);*/
 
 //Variable storing users guess to evaluate in function compare()
-var UserGuess1 = document.getElementById("UserGuess").value;
+//var UserGuess1 = document.getElementById("guess").value;
+
+//var guess1 = document.getElementById("guess").value;
 
 var answer = Math.ceil((Math.random() * 100));
+
 //function that evaluates users guess and returns feedback
 function compare()
 {
+    var guess1 = parseInt(document.getElementById("guess").value);
    // window.alert("Hello World!")
-    if (UserGuess1 === answer) 
+    if (guess1 === answer) 
     {
         document.getElementById("check").innerHTML = "Correct!";
     }
     
-    else if (UserGuess1 < answer) 
+    else if (guess1 < answer) 
     {
-        document.getElementById("check").innerHTML = "The number is lower";
+        document.getElementById("check").innerHTML = "Please guess a higher number";
     }
     else
     {
-        document.getElementById("check").innerHTML = "The number is higher";
+        document.getElementById("check").innerHTML = "Please guess a lower number";
     }
-   
 }
